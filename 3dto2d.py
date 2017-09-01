8057d4ec1c36212bb2d4d9d4e2490134abbdb035
Joshua Underwood <joshua.underwood@nrl.navy.mil>
2017-09-01 13:44:57 -0400

    CUBES CUBES CUBESgit statusgit status

--- 3dto2d.py
+++ 3dto2d.py
@@ -12,23 +12,10 @@ import shapes
 import teapot
 
 
-# points.extend(square_00.points())
-# pFill.extend(square_00.fill())
 
-squares = [ shapes.Cube((x,y,z), 2.0) 
-            for x,y,z in I.product([-10,0,10],[-10,0,10],[0]) ]
-
-# tetrahedron_00 = shapes.Tetrahedron((-5,-5,-5),(5,5,-5),(-5,5,5),(5,-5,5))
-# points.extend(tetrahedron_00.points())
-# pFill.extend(tetrahedron_00.fill(9))
-
-# tetrahedron_01 = shapes.Tetrahedron(( 5, 5, 5),(-5,-5,5),(-5,5,-5),(5,-5,-5))
-# points.extend(tetrahedron_01.points())
-# pFill.extend(tetrahedron_01.shell(9))
-
-
-# line_00 = shapes.Line((-5,-5,-5), (5,5,5))
-# points.extend(line_00.fill(20))
+N = 20
+R = 20
+squares = [ shapes.Cube((random.randint(-R,R),random.randint(-R,R),random.randint(-R,R)) , 2.0)  for _ in xrange(N) ]
 
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -84,6 +71,11 @@ def perspective_division(eye, point, plane_dist=1.0):
     e = plane_dist
     return np.array([new_point[0,0] * e/Z, new_point[0,1] * e/Z])
 
+def make_plot_points(points3d, dist = 1.0):
+    visible_points = I.ifilter(lambda p: visible_test(p, dist), points3d)
+    plot_points    = I.imap(lambda p: console_map.latlon(p[1], p[0]), visible_points)
+    return plot_points
+
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 HEIGHT, WIDTH = console_map.get_screen_size()
@@ -91,22 +83,29 @@ HEIGHT -= 1
 dist = 1.0
 eye = (0.0, 0.0, -20.0)
 
-#3Drotate the shapes
 matrix_maker   = console_map.get_screen_matrix_maker(-1.0,1.0, -1.0, 1.0, HEIGHT, WIDTH)
-for rotation in np.linspace(-8 * np.pi, 8 * np.pi, 500):
+
+rotations = [ np.array((random.randint(-10,10),random.randint(-10,10),random.randint(-10,10))) for _ in xrange(len(squares)) ]
+zipped_rotations_and_cubes = zip(rotations, squares)
+
+
+#3Drotate the shapes
+while True:
 
     #rotate shapes
-    new_points = []
-    for cube in squares:
-        cube.rotate3D(np.array([10,5,0]), cube.center)
-        new_points.extend([perspective_division(eye, point, dist) for point in cube.shell()])
+    vert_points = []
+    shll_points = []
+    for rotation, cube in zipped_rotations_and_cubes:
+        cube.rotate3D(rotation, cube.center)
+        vert_points.extend([perspective_division(eye, point, dist) for point in cube.points()])
+        shll_points.extend([perspective_division(eye, point, dist) for point in cube.shell()])
 
-    visible_points = I.ifilter(lambda p: visible_test(p, dist), new_points)
-    plot_points    = I.imap(lambda p: console_map.latlon(p[1], p[0]), visible_points)
-    
+    vert_points = make_plot_points(vert_points, dist)
+    shll_points = make_plot_points(shll_points, dist)    
+
     cntr = I.count(1)
 
-    layers = I.imap(lambda l: matrix_maker(l, next(cntr)), [plot_points])
+    layers = I.imap(lambda l: matrix_maker(l, next(cntr)), [shll_points, vert_points])
     smat = console_map.join_smat_layers_gen(layers)
 
     print colorama.Cursor.POS() + console_map.print_screen_matrix_layers(smat, symbols=' -#.'),
